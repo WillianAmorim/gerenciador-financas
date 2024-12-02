e77ce676655cdb24c243d5be20364b2f6fadc0dd
WillianAmorim <willian.amorim1@outlook.com>
2024-12-02 14:51:50 -0300

    finalizado componente header desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
 
 const App = () => {
   return (
     <div className="flex">
         <Sidebar />
+        <Header />
     </div>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,24 @@ import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { CiSettings } from "react-icons/ci";
 import { CiBellOn } from "react-icons/ci";
 import { CiSearch } from "react-icons/ci";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const Header = () => {
     return (
-        <header className="flex max-h-16 p-4 border-b-2 bg-[#F4F3EF] flex-grow justify-between">
+        <header className="flex max-h-14 p-4 border-b-2 bg-[#F4F3EF] flex-grow justify-between">
             <div className="flex items-center gap-4">
                 <IoIosArrowDropleftCircle className="text-4xl" />
                 <p>Paper Dashboard PRO React</p>
             </div>
             <div className="flex items-center gap-8 pr-8">
                 <div className="flex items-center pr-2 rounded-md bg-[#EDEDEA]">
-                    <input placeholder="Search" className="bg-[#EDEDEA] w-44 pl-2 py-2 rounded-md focus:outline-none focus:border-none" type="text" /><CiSearch />
+                    <input placeholder="Search" className="bg-[#EDEDEA] w-44 pl-2 py-1 rounded-md focus:outline-none focus:border-none" type="text" /><CiSearch />
                 </div>
-                <CiBellOn />
-                <CiSettings />
+                <div className="flex gap-1">
+                    <CiBellOn className="text-xl" />
+                    <IoMdArrowDropdown />
+                </div>
+                <CiSettings className="text-xl" />
             </div>
         </header>
     )
